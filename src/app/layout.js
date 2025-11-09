ada2039a8d6bbcd1d4b98b28deea3ec55f38cdf5
rick <rohit1322saini@gmail.com>
2025-11-09 12:47:58 +0000

    ssr things

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,6 +21,11 @@ export default function RootLayout({ children }) {
           integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
           crossOrigin="anonymous"
         />
+
+<title>
+At TechInve, we’re building the most comprehensive platform to explore, organize, and stay updated on modern technologies. Whether you're a developer, founder, or just a curious learner, we help you track and understand the evolving world of tech — from frameworks and tools to trends and best practices.</title>
+
+
       </head>
       <body>
         <Navbar />
